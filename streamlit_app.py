fc81ebdd6fdf3f21f4bd5f309b9b0fb113544869
Aurel9234130 <128035479+Aurel9234130@users.noreply.github.com>
2023-03-16 17:03:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,33 +59,35 @@ except URLError as e:
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+   
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-#New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
-      
-except URLError as e:
-   streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
+   
+      
+
+   
+
+
+
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
+
+
 #streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+
 
 #New Section to display another fruityvice api response
 fruit_choice_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')
